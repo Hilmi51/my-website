9c1d63a78ffa57c032f628f2e47ee8ad4e3bc1cd
Hilmi ŞAFAK <82928954+Hilmi51@users.noreply.github.com>
2023-11-09 18:47:38 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
 import "./App.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faInstagram, faLinkedin, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {Button} from "@material-tailwind/react";
 
 function App() {
     return (
         <main className="container">
+            <section className="profile">
+                <img
+                    src={require("./images/IMG_20220816_164712_128.jpg")}
+                    alt="My Profil Photo"
+                    width="350"
+                    height="350"
+                />
+                <div className="profile-text">
+                    <h1>HILMI SAFAK</h1>
+                    <h4><span>BACKEND DEVELOPER</span></h4>
+                    <div className="icons">
+                        <a
+                            href="https://www.instagram.com/hilmi_safak/"
+                            target="_blank"
+                            className="i ai"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faInstagram}/>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/hilmi-%C5%9Fafak-40046a210/"
+                            target="_blank"
+                            className="i ai"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faLinkedin}/>
+                        </a>
+                        <a
+                            href="https://twitter.com/Hilmi511"
+                            target="_blank"
+                            className="i ai"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faTwitter}/>
+                        </a>
+                        <a
+                            href="https://github.com/Hilmi51"
+                            target="_blank"
+                            className="i ai"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faGithub}/>
+                        </a>
+                        <a
+                            href="https://www.youtube.com/channel/UClQQsXPjHynuY4A7X18eNKw"
+                            target="_blank"
+                            className="i ai"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={faYoutube}/>
+                        </a>
+                    </div>
+                    <div className="hire-me">
+                        <button onClick={() => (window.location = "mailto:hilmisafak.info@gmail.com")}  target="_blank">
+                            Contact Me Now !
+                        </button>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ThemeProvider } from 'next-themes'
+import {ThemeProvider} from 'next-themes'
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
 root.render(
-  <React.StrictMode>
-    <ThemeProvider attribute="class">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider attribute="class">
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
 );
